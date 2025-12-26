baba2ec0917dbf4d25413d43f8caf54c8735c610
aysha3f <tahiaysha220@gmail.com>
2025-12-26 10:03:13 +0600

    Create Navbar for Book Vibe

---
+++ src/pages/Root/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router';
+import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Header/Navbar';
+
+
+const Root = () => {
+    return (
+        <div>
+      <Navbar></Navbar>
+            <Outlet></Outlet>
+       <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
