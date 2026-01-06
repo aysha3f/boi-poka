0a5fd70ed0e21837c6478f0ab33a869cfb6f6f60
aysha3f <tahiaysha220@gmail.com>
2026-01-06 21:15:42 +0600

    Deploy on Netlify and Surge

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoredDB } from '../../utility/addToDB';
+   import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
+ import { ToastContainer, toast } from 'react-toastify';
 
 const BookDetails = () => {
     const {id} = useParams();
     const bookId = parseInt(id);
     const data = useLoaderData();
    const singleBook= data.find(book=>book.bookId ===bookId)
-   const {bookName, image} =singleBook;
-    
+   const {bookName, image} =singleBook ||{};    
 const handleMarkAsRead = id=>{
     // store with id
     // where to store
@@ -16,6 +20,13 @@ const handleMarkAsRead = id=>{
     // if book already exit then show a alert
     // if book not exist then push in the collection or array
 
+// MySwal.fire({
+//   title: "Good job!",
+//   text: "You clicked the button!",
+//   icon: "success"
+// });
+
+ toast("Wow so easy!")
     addToStoredDB(id)
 }
 
@@ -23,7 +34,7 @@ const handleMarkAsRead = id=>{
         <div className='w-2/3 mx-auto'>
       <img className='w-48' src={image} alt="" />
       <h5>{bookName}</h5>
-
+ <ToastContainer />
       <button onClick={() => handleMarkAsRead(id)} className="btn btn-accent m-2">Mark as Read</button>
 <button className="btn btn-info m-2">Add To WishList</button>
         </div>
